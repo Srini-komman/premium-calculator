e7682661cd8754c1d8fe56eaf88abc3dc49ec63f
Srini Komman <srini.komman@gmail.com>
2020-10-19 15:16:16 +1100

    UnitTest project has been added
    MediatR pattern has been implemented for handling queries and asynchronous calls to the data store

--- premium.calculator.api/Premium.Calculator.API/Controllers/CustomerController.cs
+++ premium.calculator.api/Premium.Calculator.API/Controllers/CustomerController.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Premium.Calculator.Application.Customers;
 using Premium.Calculator.Domain;
 using Premium.Calculator.Persistence.Repositories;
 
+
 namespace Premium.Calculator.API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class CustomerController : ControllerBase
     {
-        private readonly UnitOfWork unitOfWork;
-        public CustomerController(IUnitOfWork unitOfWork)
+        
+        private readonly IMediator mediator;
+        public CustomerController(IMediator mediator)
         {
-            this.unitOfWork = unitOfWork as UnitOfWork;
+            this.mediator = mediator;
         }
         // GET api/values
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Customer>>> Get()
+        public async Task<ActionResult<List<Customer>>> List()
         {
-            List<Customer> customers = await this.unitOfWork.CustomerRepository.GetCustomerWithOccupationAsync();
+            List<Customer> customers = await this.mediator.Send(new List.Query());
 
             return Ok(customers);
         }        
---
+++ premium.calculator.api/Premium.Calculator.Application/Customers/List.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Premium.Calculator.Domain;
+using Premium.Calculator.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Premium.Calculator.Application.Customers
+{
+    public class List
+    {
+        public class Query: IRequest<List<Customer>> { }
+
+        public class Handler : IRequestHandler<Query, List<Customer>>
+        {
+            private readonly UnitOfWork unitOfWork;
+            public Handler(IUnitOfWork unitOfWork)
+            {
+                this.unitOfWork = unitOfWork as UnitOfWork;
+            }
+            public async Task<List<Customer>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                List<Customer> customers = await this.unitOfWork.CustomerRepository.GetCustomerWithOccupationAsync();
+                return customers;
+            }
+        }
+    }
+}
